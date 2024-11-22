1858c1fa1653c08b3446dfee5148c3728bad34d4
Bilal Bayasut <bilalbayasut@gmail.com>
2024-11-22 08:20:27 +0000

    wip web form

--- frappeair/frappeair/doctype/airplane_flight/airplane_flight.py
+++ frappeair/frappeair/doctype/airplane_flight/airplane_flight.py
@@ -2,9 +2,15 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class AirplaneFlight(Document):
+class AirplaneFlight(WebsiteGenerator):
 	def on_submit(self):
 		self.status = "Completed"
+
+	def before_validate(self):
+		if not self.route:
+			self.route = f"airplane-flights/{self.name}"
+		# if self.status == "Scheduled":
+		# 	self.is_published = True

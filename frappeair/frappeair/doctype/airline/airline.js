bb27d3bf6c628166f50627ca1a52d99b0acfafdd
Bilal Bayasut <bilalbayasut@gmail.com>
2024-11-23 13:54:20 +0000

    added airline website menu

--- frappeair/frappeair/doctype/airline/airline.js
+++ frappeair/frappeair/doctype/airline/airline.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2024, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airline", {
+	refresh(frm) {
+        if (frm.doc.website) {
+            frm.add_custom_button('Visit Website', function () {
+                window.open(frm.doc.website, '_blank');
+            }).addClass('btn-primary');
+        }
+	},
+});

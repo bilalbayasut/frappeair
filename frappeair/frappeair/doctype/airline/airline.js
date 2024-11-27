3ea52bb69ed6b67d2d03b2f5e3ad4f24569f4a7d
Bilal Bayasut <bilalbayasut@gmail.com>
2024-11-27 02:26:08 +0000

    fix airplane by airline report

--- frappeair/frappeair/doctype/airline/airline.js
+++ frappeair/frappeair/doctype/airline/airline.js
@@ -4,9 +4,6 @@
 frappe.ui.form.on("Airline", {
 	refresh(frm) {
         if (frm.doc.website) {
-            // frm.add_custom_button('Visit Website', function () {
-            //     window.open(frm.doc.website, '_blank');
-            // }).addClass('btn-primary');
             frm.add_web_link(frm.doc.website, "Visit Website");
         }
 	},
